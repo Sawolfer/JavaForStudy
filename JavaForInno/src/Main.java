38ecb8650c977c31a016facbd18e0012d6132e20
Sawolf <Savipon@yandex.ru>
2023-10-24 15:46:36 +0300

    app in terminal

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -1,17 +1,84 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static Scanner sc = new Scanner(System.in);
+    public static void Print(String message){
+        System.out.printf(message + "\n");
+    }
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void PrintMenu(){
+        Print("[0] – exit");
+        Print("[1] – print current string");
+        Print("[2] – append the string with a string that the user will input");
+        Print("[3] – insert a string that the user will input to the current string at a certain index");
+        Print("[4] – reverse current string");
+        Print("[5] – given indexes l and r delete substring in the range l to r from the current string");
+        Print("[6] – given indexes l and r replace substring in the range l to r from the current string with a string that will be given in the input");
+    }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args){
+        PrintMenu();
+
+        StringBuilder currentString = new StringBuilder("Test");
+        boolean working = true;
+        while (working){
+            String option = sc.nextLine();
+
+
+            switch (option){
+                case ("0"):
+                    working = false;
+                    return;
+                case ("1"):
+                    Print(currentString.toString());
+                    PrintMenu();
+                    break;
+                case("2"):
+                    String newStringTMP = sc.nextLine();
+                    currentString.append(newStringTMP);
+                    Print(currentString.toString());
+                    PrintMenu();
+                    break;
+                case ("3"):
+                    int location = sc.nextInt();;
+                    String newPiece1 = sc.nextLine();
+
+                    currentString.insert(location, newPiece1);
+                    Print(currentString.toString());
+                    PrintMenu();
+                    break;
+                case ("4"):
+                    currentString.reverse();
+                    Print(currentString.toString());
+                    PrintMenu();
+                case ("5"):
+                    int l = sc.nextInt();
+                    int r = sc.nextInt();
+
+                    currentString.delete(l, r);
+                    Print(currentString.toString());
+                    PrintMenu();
+                    break;
+                case ("6"):
+                    int l1 = sc.nextInt();
+                    int r1 = sc.nextInt();
+                    String newPiece = sc.nextLine();
+
+                    currentString.replace(l1, r1, newPiece);
+                    Print(currentString.toString());
+                    PrintMenu();
+                    break;
+                default:
+                    Print("Incorrect input. Try another values");
+                    PrintMenu();
+                    break;
+            }
+
+
         }
     }
+
+
 }
