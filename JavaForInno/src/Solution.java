8f967dc246c1f47f2ef73ddb379433be4a0b557a
Sawolf <Savipon@yandex.ru>
2024-01-04 18:08:18 +0300

    govno

--- JavaForInno/src/Solution.java
+++ JavaForInno/src/Solution.java
@@ -1,16 +1,35 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-public class Solution {
-    public static void main(String[] args){
-        double  answer = Math.pow(2, 3);
-        System.out.print(answer);
+import java.util.Scanner;
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+    static int maxAns = 0;
+    public static void main(String args[]){
+        String str = sc.nextLine();
+
+        for (int i=1; i<(str.length()-1); i++){
+            String tmpString = str.substring(i);
+            int currentLength = lengthOfLongestSubstring(tmpString);
+            maxAns = Math.max(maxAns, currentLength);
+
+        }
+
+        System.out.println(maxAns);
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        int answer =0;
+        for (int i = 0; i < (s.length()-1); i++ ){
+            String next = s.substring(i, i+1);
+            if (s.substring(0, i).contains(next)){
+                answer = i;
+                break;
+            }
+            else {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
