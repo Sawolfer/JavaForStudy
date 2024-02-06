d8ef20440ac6f8e6159bfdb9593a8342686c1c1d
Sawolf <Savipon@yandex.ru>
2024-02-06 18:47:18 +0300

    Spell checker

--- JavaForInno/src/SpellChecker.java
+++ JavaForInno/src/SpellChecker.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class FrequencyAnalysis {
+public class  SpellChecker {
 
     static Scanner sc;
     public static void main(String[] args){
@@ -11,6 +11,7 @@ public class FrequencyAnalysis {
         String inputString = sc.nextLine();
         String[] words = inputString.split(" ");
         HashMap<String, Integer> wordsStorage = new HashMap<>(n);
+
         for (String word : words){
             if (wordsStorage.get(word) != null){
                 int tmp = wordsStorage.get(word);
@@ -19,21 +20,28 @@ public class FrequencyAnalysis {
                 wordsStorage.put(word, 1);
             }
         }
-        List<KeyValuePair<String, Integer>> answers = wordsStorage.entrySet();
-        Collections.sort(answers, new Comparator<KeyValuePair<String, Integer>>() {
-            @Override
-            public int compare(KeyValuePair<String, Integer> o1, KeyValuePair<String, Integer> o2) {
-                int result = o2.value.compareTo(o1.value);
-                if (result == 0) {
-                    result = o1.key.compareTo(o2.key);
-                }
-                return result;
+
+        n = Integer.parseInt(sc.nextLine());
+        HashMap<String, Integer> wordsStorage2 = new HashMap<>(n);
+        ArrayList<String> answer = new ArrayList<>();
+        inputString = sc.nextLine();
+        String[] words2 = inputString.split(" ");
+        for(String word : words2) {
+            if (wordsStorage.get(word) == null) {
+                wordsStorage2.put(word, 1);
             }
-        });
+        }
+        List<KeyValuePair<String, Integer>> answer2 = wordsStorage2.entrySet();
+        for(String word : words2){
+            if (wordsStorage2.get(word)!= null){
+                answer.add(word);
+                wordsStorage2.remove(word);
+            }
+        }
 
-
-        for (KeyValuePair<String, Integer> item : answers){
-            System.out.println(item.key + " " + item.value);
+        System.out.println(answer.size());
+        for (String item : answer) {
+            System.out.println(item);
         }
     }
 }
@@ -65,7 +73,7 @@ class HashMap<K, V> implements Map<K, V> {
     public HashMap(int capacity){
         this.size = 0;
         this.capacity = capacity;
-        this.hashTable = new List[1000];
+        this.hashTable = new List[capacity];
         for (int i = 0; i < capacity; i++){
             this.hashTable[i] = new LinkedList<>();
         }
@@ -75,7 +83,7 @@ class HashMap<K, V> implements Map<K, V> {
     public KeyValuePair<K, V> getEntry(K key) {
         int hash = Math.abs(key.hashCode()) % capacity;
         for (int i = 0; i < this.hashTable[hash].size(); i++){
-            KeyValuePair tmpPair = hashTable[hash].get(i);
+            KeyValuePair<K, V> tmpPair = hashTable[hash].get(i);
             if (tmpPair.key.equals(key)){
                 return tmpPair;
             }
@@ -87,7 +95,7 @@ class HashMap<K, V> implements Map<K, V> {
     public V get(K key) {
         V value = null;
         if (getEntry(key) != null){
-            value = (V)(getEntry(key).value);
+            value = (getEntry(key).value);
         }
         return value;
     }
@@ -119,15 +127,11 @@ class HashMap<K, V> implements Map<K, V> {
     @Override
     public List<KeyValuePair<K, V>> entrySet(){
         List<KeyValuePair<K, V>> returnable = new LinkedList<>();
-        for ( int i = 0; i < hashTable.length; i++){
-            if (hashTable[i] != null){
-                for (int j = 0; j < hashTable[i].size(); j++){
-                    returnable.add(hashTable[i].get(j));
-                }
+        for (List<KeyValuePair<K, V>> pairs : hashTable) {
+            if (pairs != null) {
+                returnable.addAll(pairs);
             }
-
         }
-
         return returnable;
     }
 }
