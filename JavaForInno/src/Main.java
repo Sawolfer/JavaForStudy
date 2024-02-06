eee2ac3032794f5e0251d0fd83b4bfd93ec5dc40
Sawolf <Savipon@yandex.ru>
2024-02-06 22:08:15 +0300

    asdasd

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         sc = new Scanner(System.in);
-        for (int i = 0; i< 12; i++){
+        for (int i = 0; i< 13; i++){
             int n = sc.nextInt();
             System.out.print((n*n - 2*n + 7)%13 + " ");
         }
