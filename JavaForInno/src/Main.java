4abbdfc795cf7acbef7c4c1e03d7ca01f4548008
Sawolf <Savipon@yandex.ru>
2024-02-06 19:09:12 +0300

    padasda

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sc;
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        sc = new Scanner(System.in);
+        for (int i = 0; i< 12; i++){
+            int n = sc.nextInt();
+            System.out.print((n*n - 2*n + 7)%13 + " ");
         }
+
     }
 }
