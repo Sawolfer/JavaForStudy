1db555db07dd0077cb384dcaf0e5603b72ff13c6
Sawolf <Savipon@yandex.ru>
2024-02-19 18:27:53 +0300

    bullshit

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,11 +6,35 @@ public class Main {
     static Scanner sc;
     public static void main(String[] args) {
 
+        ArrayList<String> alphabet = new ArrayList<>();
+        int[] outputCounts = new int[3];
         sc = new Scanner(System.in);
-        for (int i = 0; i< 13; i++){
-            int n = sc.nextInt();
-            System.out.print((n*n - 2*n + 7)%13 + " ");
+        int M = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < M; i++) {
+            alphabet.add(sc.next());
+        }
+        for (int i = 0; i < 3; i++){
+            int tmp = Integer.parseInt(sc.next());
+            outputCounts[i] = tmp;
+        }
+        ArrayList<String> L1 = new ArrayList<>();
+        ArrayList<String> L2 = new ArrayList<>();
+        ArrayList<String> L3 = new ArrayList<>();
+
+        L1.add("_");
+
+        for (int i = 2; i < outputCounts[0]; i++){
+            StringBuilder word = new StringBuilder();
+
+            for (String letter : alphabet){
+                
+            }
         }
 
+        for (String words : L1){
+            System.out.println(words);
+        }
     }
+
+
 }
