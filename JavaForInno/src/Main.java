8c2fe22090ef43e533f9f1ea7eac191a50c836d1
Sawolf <Savipon@yandex.ru>
2024-02-20 15:08:46 +0300

    smth

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.ref.SoftReference;
 import java.util.Scanner;
 
 class RestoringSpaces{
@@ -14,8 +15,29 @@ class RestoringSpaces{
         for( int i = 0; i < countWords; i++){
             inputWords[i] = sc.next();
         }
+        String inputString = sc.next();
 
-        
+        for (int i = 0; i < countLetters+1; i++) {
+            dp[i] = 0;
+        }
+        int lastPos = 0;
+        for(int i = 0; i < countLetters+1; i++){
+
+            for (String word : inputWords){
+                if((i - word.length() - lastPos) >= 0 && inputString.substring((i - word.length()), i).equals(word)){
+                    dp[i] = 1;
+                    lastPos = i;
+                }
+            }
+        }
+        for (int i = 0; i < inputString.length(); i++){
+            if (dp[i] == 0){
+                System.out.print(inputString.charAt(i));
+            } else {
+                System.out.print(" " + inputString.charAt(i));
+            }
+
+        }
 
     }
 }
