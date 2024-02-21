1556721f7b2296c03bdbc83046e46b877b2a5d31
BrainPumpkin <97783852+Sawolfer@users.noreply.github.com>
2024-02-21 13:08:22 +0000

    some

--- JavaForInno/src/B.java
+++ JavaForInno/src/B.java
@@ -22,7 +22,19 @@ class Knapsack {
             int tmp = sc.nextInt();
             costs[i] = tmp;
         }
-        
+    
+        int[][] K = new int[n+1][n+1];
+        K[0][0]= 0;
+        for (int i = 1; i <=n; i++){
+            for (int j = 1; j <= n; j++){
+                if (weights[i-1] <= j){
+                    K[i][j] = Math.max(costs[i-1] + K[i-1][j - weights[i - 1]], K[i-1][j]);
+                }
+                else {
+                    K[i][j] = K[i-1][j];
+                }
+            }
+        }
 
 
     }
