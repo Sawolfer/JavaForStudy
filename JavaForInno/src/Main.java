93466ed816e07232628750bbc581963e8d8265e8
Sawolf <Savipon@yandex.ru>
2024-02-23 13:22:06 +0300

    genearator

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner sc;
+    static int index = 0;
+    static ArrayList<String> alphabet = new ArrayList<>();
     public static void main(String[] args) {
 
-        ArrayList<String> alphabet = new ArrayList<>();
+
         int[] outputCounts = new int[3];
         sc = new Scanner(System.in);
         int M = Integer.parseInt(sc.nextLine());
@@ -23,24 +25,27 @@ public class Main {
         ArrayList<String> L3 = new ArrayList<>();
 
         L1.add("_");
-        StringBuilder tmp = new StringBuilder();
-        generateCombinations(alphabet, tmp, L1, 0, 4);
+
+        for (int i = 0; i <= 4; i++){
+            String tmp = generateCombinations();
+            L1.add(tmp);
+        }
 
         for (String words : L1){
             System.out.println(words);
         }
     }
-    public static void generateCombinations(ArrayList<String> inputArray, StringBuilder currentCombination, List <String> output, int start, int k) {
-        if (k == 0) {
-            output.add(currentCombination.toString());
-            return;
-        }
+    public static String generateCombinations() {
 
-        for (int i = start; i < inputArray.size(); i++) {
-            currentCombination.append(inputArray.get(i));
-            generateCombinations(inputArray, currentCombination, output, i, k - 1);
-            currentCombination.deleteCharAt(currentCombination.length() - 1);
+        StringBuilder word = new StringBuilder();
+        int tmp = index;
+
+        while (tmp > 0){
+            word.append(alphabet.get(tmp % alphabet.size()));
+            tmp = tmp / alphabet.size();
         }
+        index +=1;
+        return word.toString();
     }
 
 
