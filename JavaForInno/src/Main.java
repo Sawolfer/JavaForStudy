27f6aa1a0d9b7a3fc35d9730eb21167f2ebba4a5
Sawolf <Savipon@yandex.ru>
2024-02-27 17:04:34 +0300

    middle

--- JavaForInno/src/Main.java
+++ JavaForInno/src/Main.java
@@ -1,6 +1,10 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
-public class Main {
+public class Main  {
 
     static Scanner sc;
     public static void main(String[] args) {
@@ -12,4 +16,61 @@ public class Main {
         }
 
     }
+
+    public static <T> ArrayList<T> SavvaPonomarev_bucket_srt(T[] arr){
+
+        if (arr == null || arr.length == 0){
+            return;
+        }
+
+        int numberOfBuckets = arr.length;
+        ArrayList<ArrayList<T>> buckets = new ArrayList<>(numberOfBuckets);
+
+        for (int i = 0; i < numberOfBuckets; i++){
+            ArrayList<T> tmp = new ArrayList<>();
+            buckets.add(tmp);
+        }
+
+        for (T item : arr){
+            int index = (int)( item.hashCode() % numberOfBuckets );
+            buckets.get(index).add(item);
+        }
+
+
+
+    }
+
+    public static <T extends Comparable<T>> ArrayList<T> SavvaPonomarev_count_sort(T[] arr){
+        int maxi = (int) maxT(arr);
+        int[] count = new int[ maxi + 1];
+        T[] answer = Arrays.copyOf(arr, arr.length);
+
+        for (T item : arr){
+            count[(item.hashCode())%(count.length) + 1]++;
+        }
+        for (int i = 0; i < maxi; i++){
+            count[i] += count[i-1];
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            
+        }
+
+    }
+    private static <T extends Comparable<T>> T maxT(T[] arr){
+
+        T answer = arr[0];
+        for (T item : arr){
+            if (item.compareTo(answer) >= 1){
+                answer = item;
+            }
+        }
+        return answer;
+    }
+
 }
+
+
+
+
+
